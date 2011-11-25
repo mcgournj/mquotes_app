877549252c3782b1aa557fccb55bee621753b9a4
Joe McGouran <joe_mcgouran@yahoo.ie>
2011-11-25 17:02:52 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MquotesApp::Application.config.secret_token = '13ca7fd623d7dc067cdc11263ccaed9fabde28f687812e62ad3446022218f776b268b69fc38f5ef90dc0348a895e7e2b556b8acd608c7126421b8c6fcb3bda21'
